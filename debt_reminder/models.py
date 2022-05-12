a463cd5ccf2d3d9dfe782683cbb46854efe7b9d4
nathan <quangnhan145@gmail.com>
2022-05-12 20:05:44 +0700

    Fixed

--- debt_reminder/models.py
+++ debt_reminder/models.py
@@ -4,13 +4,17 @@ from django.urls import reverse
 # Create your models here.
 
 class Customer(models.Model):
+    CHOICE_STATUS = [
+        ('normal', "Normal"),
+        ('overdue', "Overdue")
+    ]
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     debt = models.FloatField()
     due_date = models.DateField()
-    is_overdued = models.BooleanField(default=False)
+    status = models.CharField(max_length=50, default="normal", choices=CHOICE_STATUS)
     is_reminded = models.BooleanField(default=False)
 
     def get_absolute_url (self):
-        return reverse('customer_list', args=[str(self.id)])
+        return reverse('customer_list')
